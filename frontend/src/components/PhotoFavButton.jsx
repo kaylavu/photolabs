9c60112be1e06ac62f218b41be5a63198d8b1fe1
Kayla Vu <kaylaavu@gmail.com>
2024-02-02 12:01:57 -0800

    display photos and add toggle fav photo button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,15 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [favouritePhoto, setFavouritePhto] = useState(false);
+
+  const toggleFavouritePhto = () => {
+    setFavouritePhto((prev) => !prev);
+  };
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={toggleFavouritePhto}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={favouritePhoto} />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/PhotoListItem.scss";
+import '../styles/PhotoListItem.scss';
+import PhotoFavButton from 'components/PhotoFavButton';
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <div className="photo-list__item">
+      <PhotoFavButton />
+      <img className="photo-list__image" src={props.photo.imageSource}></img>
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.photo.profile} />
+        <div className="photo-list__user-info">
+          {props.photo.username}
+          <div className="photo-list__user-location">
+            {props.photo.location.city}, {props.photo.location.country}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
